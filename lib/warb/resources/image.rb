83920a0da46b94c5dd491f432f6d7c0a48907692
Rosemilson <rosemilson.ribeiro@rebase.com.br>
2025-06-05 18:40:03 -0300

    feat: add errors handling for Warb::Resources::Image

--- lib/warb/resources/image.rb
+++ lib/warb/resources/image.rb
@@ -4,10 +4,14 @@ module Warb
   module Resources
     class Image < MediaResource
       def build_header
+        check_errors
+
         common_media_params(:image)
       end
 
       def build_payload
+        check_errors
+
         common_media_params(:image, with_caption: true)
       end
     end
