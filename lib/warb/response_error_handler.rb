829928c0b02c2c81b5206b6bd67258e0c69742ed
james_macena <james.macena@rebase.com.br>
2025-08-22 15:50:30 -0300

    fix: change error message

--- lib/warb/response_error_handler.rb
+++ lib/warb/response_error_handler.rb
@@ -14,7 +14,7 @@ module Warb
       error_class  = custom_class || http_class || Warb::RequestError
       Warb.configuration.logger.error(message.to_s)
 
-      raise error_class, message_from_error
+      raise error_class, message
     end
 
     private
@@ -23,20 +23,12 @@ module Warb
       Warb.configuration.custom_errors[@status]&.dig(code)
     end
 
-    def message_from_error
-      details || message || "HTTP #{@status}"
-    end
-
     def code
       @code ||= @body.dig('error', 'code')
     end
 
     def message
       @message ||= @body.dig('error', 'message')
-    end
-
-    def details
-      @details ||= @body.dig('error', 'error_data', 'details')
     end
   end
 end
